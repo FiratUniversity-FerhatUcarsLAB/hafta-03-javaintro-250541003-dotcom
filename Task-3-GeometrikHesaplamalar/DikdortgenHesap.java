58d264929a7a7f52e9e4936d9ce985570508407f
250541003-dotcom <250541003@firat.edu.tr>
2025-10-21 16:23:20 +0300

    Add DıkdortgenHesap class for rectangle calculations

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,17 @@
+package odev;
+
+public class DıkdortgenHesap {
+
+	public static void main(String[] args) {
+		double a=4.5;
+		double b=7.9;
+		double alan=a*b;
+		double cevre=2*(a+b);
+		System.out.println("alan="+alan);
+		System.out.println("cevre="+cevre);
+		
+		
+			
+	}
+
+}
