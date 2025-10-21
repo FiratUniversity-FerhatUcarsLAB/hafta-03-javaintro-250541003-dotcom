abdca6e6e10cc61a1b7f54ad6eb9620edb2d95cc
250541003-dotcom <250541003@firat.edu.tr>
2025-10-21 15:14:56 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,11 @@
+package odev;
+
+public class üçmesaj {
+
+	public static void main(String[] args) {
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+		System.out.println("Programlama Eğlencelidir");
+	}
+
+}
