91dbbeffeb6493bc0418518dde8a026c29367b3b
250541003-dotcom <250541003@firat.edu.tr>
2025-10-22 00:11:00 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+package odev;
+
+public class IsimKartı {
+
+	public static void main(String[] args) {
+ String ad=" Melike";
+		 String soyad="Keke";
+		 String ogrNo="250541003";
+		 String bolum="Yazilim Muh";
+        System.out.println("==================");
+		System.out.println("ÖĞRENCİ BİLGİLERİ");
+		System.out.println("==================");
+		System.out.println("Ad:"+ad);
+		System.out.println("Soyad:"+soyad);
+		System.out.println("No:"+ogrNo);
+		System.out.println("Bolum:"+bolum);
+		System.out.println("===================");
+	}
+
+}
