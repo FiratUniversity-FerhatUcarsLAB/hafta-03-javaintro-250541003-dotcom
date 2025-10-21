f2758787076ee5d6c92513666dfbf16006972778
250541003-dotcom <250541003@firat.edu.tr>
2025-10-21 17:24:28 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,13 @@
+package odev;
+
+public class beşkeretekrar {
+
+	public static void main(String[] args) {
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+	}
+
+}
