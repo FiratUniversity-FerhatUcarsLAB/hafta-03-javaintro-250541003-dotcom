e32b9c849742a60a27d18915d2595f22e3349c6c
250541003-dotcom <250541003@firat.edu.tr>
2025-10-21 18:05:29 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,12 @@
+package odev;
+
+public class ToplamaSeri {
+
+	public static void main(String[] args) {
+		int toplama=1+2+3+4+5+6+7+8+9;
+	System.out.println("1+2+3+4+5+6+7+8+9= "+toplama);
+	
+
+	}
+
+}
