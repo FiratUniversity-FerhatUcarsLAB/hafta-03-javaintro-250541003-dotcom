e8626b896b8e679076dc1977a5b7278257a5f2a3
250541003-dotcom <250541003@firat.edu.tr>
2025-10-22 00:42:15 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,14 @@
+package odev;
+
+public class HesapOzetı {
+
+	public static void main(String[] args) {
+		System.out.println("Urun    Miktar   Fıyat   " );
+		System.out.println("Ekmek    1         10");
+		System.out.println("Yumurta  3         80");
+		System.out.println("Çıkolata 4         75");
+		System.out.println("=====================");
+
+	}
+
+}
