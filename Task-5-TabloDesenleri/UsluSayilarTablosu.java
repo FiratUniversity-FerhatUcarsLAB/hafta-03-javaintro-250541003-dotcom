5f50c3e6543b391f81c5b17eb0e82a92d172124b
250541003-dotcom <250541003@firat.edu.tr>
2025-10-22 10:15:45 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,12 @@
+package odev;
+
+public class UsluSayılarTablosu {
+	public static void main(String[] args) {
+        System.out.println("a   a^2   a^3");
+        System.out.println("-----------------");
+
+        for (int a = 1; a <= 5; a++) {
+            System.out.println(a + "   " + (a * a) + "     " + (a * a * a));
+        }
+    }
+}
