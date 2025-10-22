b4f292b32f4552812e8489132c15ee51a96a89b1
250541003-dotcom <250541003@firat.edu.tr>
2025-10-22 09:58:24 +0300

    Add multiplication table for 2

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,15 @@
+package odev;
+
+public class CarpımTablosu {
+
+	public static void main(String[] args) {
+	System.out.println("2'nin Carpım Tablosu");
+	System.out.println("======================");
+	for (int i=1;i<=10; i++) {
+		System.out.println("2 ×"+i+"="+(2*i));
+		
+	}
+
+	}
+
+}
