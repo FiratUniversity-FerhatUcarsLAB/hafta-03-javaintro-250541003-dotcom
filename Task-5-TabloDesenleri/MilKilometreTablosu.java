7f0baa133773ce7e26369fb4a32b9206f33fea1c
250541003-dotcom <250541003@firat.edu.tr>
2025-10-22 10:23:36 +0300

    Add Mil to Kilometer conversion table
    This program prints a conversion table from miles to kilometers.

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,19 @@
+package odev;
+
+public class MilKilometreTablosu {
+
+	public static void main(String[] args) {
+		System.out.println("MilKilometre");
+        System.out.println("---------------------");
+
+        System.out.println("1      " + (1 * 1.609));
+        System.out.println("5      " + (5 * 1.609));
+        System.out.println("10     " + (10 * 1.609));
+        System.out.println("20     " + (20 * 1.609));
+        System.out.println("50     " + (50 * 1.609));
+    }
+
+
+	}
+
+
