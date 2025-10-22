81ed7359d337c4e93b6db0387dd2dc5ccce58a20
250541003-dotcom <250541003@firat.edu.tr>
2025-10-22 20:40:08 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,12 @@
+package odev;
+
+public class AsciiArtIsim {
+
+	public static void main(String[] args) {
+		System.out.println(" __   __  ______ __     __ _  __  ______     ");
+        System.out.println("|  \\/  ||_ ___|| |     | || | //|   ___|  ");
+        System.out.println("| |\\/  || __|  | |     | || |// |   __|     ");
+        System.out.println("| |  |  || |____| |____ | || |\\ |  |___    ");
+        System.out.println("|_|  |_ ||_____||______||_||_| \\|______|   ");
+	}  
+}
